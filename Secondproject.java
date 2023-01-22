994de91854d034c09e4bb0368fdbe368031f3216
Sbalasubramaniyan <98730786+Sbalasubramaniyan@users.noreply.github.com>
2023-01-22 19:49:57 +0530

    Update Secondproject.java

--- Secondproject.java
+++ Secondproject.java
@@ -3,6 +3,6 @@ public class Secondproject{
     public static void main(String[] args)
             {
                System.out.println("Hello World 2");
-               System.out.println("GIT");
+               System.out.println("GIT m");
              }
 }
